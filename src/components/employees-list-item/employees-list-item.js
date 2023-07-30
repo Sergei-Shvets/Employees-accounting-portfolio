bd3541e8b53765c8a6cad5dda0896071a30e9381
Sergei <shvets555@mail.ru>
2023-07-30 20:39:13 +0600

    employees and their states added

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,12 +1,56 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+// Классовый компонент
+
+class EmployeesListItem extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            increase: false,
+            like: false,
+        }
+    }
+
+    onIncrease = () => {
+        this.setState(state => ({
+            increase: !state.increase,
+        }))
+        // Можно использовать деструктуризацию:
+        // this.setState(({increase}) => ({
+        //     increase: !increase
+        // }))
+    }
+
+    addLike = () => {
+        this.setState(state => ({
+            like: !state.like,
+        }))
+    }
+
+    render () {
+        const {name, salary} = this.props;
+        const {increase, like} = this.state;
+
+        let classNames = "list-group-item d-flex justify-content-between";
+
+    if(increase){
+        classNames += " increase";
+    }
+
+    if(like){
+        classNames += " like";
+    }
+
     return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
+        <li className={classNames}>
+            <span className="list-group-item-label"
+                onClick={this.addLike}>{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-cookie btn-sm" type="button">
+                <button className="btn-cookie btn-sm"
+                    type="button"
+                    onClick={this.onIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
                 <button className="btn-trash btn-sm" type="button">
@@ -16,6 +60,35 @@ const EmployeesListItem = () => {
             </div>
         </li>
     )
+    }
 }
+
+// Функциональный компонент
+
+// const EmployeesListItem = (props) => {
+//     const {name, salary, increase} = props;
+
+//     let classNames = "list-group-item d-flex justify-content-between";
+
+//     if(increase){
+//         classNames += " increase";
+//     }
+    
+//     return (
+//         <li className={classNames}>
+//             <span className="list-group-item-label">{name}</span>
+//             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+//             <div className="d-flex justify-content-center align-items-center">
+//                 <button className="btn-cookie btn-sm" type="button">
+//                     <i className="fas fa-cookie"></i>
+//                 </button>
+//                 <button className="btn-trash btn-sm" type="button">
+//                     <i className="fas fa-trash"></i>
+//                 </button>
+//                 <i className="fas fa-star"></i>
+//             </div>
+//         </li>
+//     )
+// }
 
 export default EmployeesListItem;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,22 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+  const elements = data.map((item) => {
+    return(
+      <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase} key={item.id}/>
+      // Здесь также можно использовать спред-оператор. Для этого сначала делаем деструктуризацию, прямо внутри метода map, до метода return
+      // Выглядит это так: const {id, ...itemProps}; 
+      // В itemProps передаются все остальные свойства 
+      // свойство id обязательно нужно вытащить отдельно, чтобы присвоить его свойству 'key' каждого элемента
+      // Затем уже идет присваивание: <EmployeesListItem key={id} {...itemProps};
+      // Как видно, остальные свойства добавляются через спред-оператор. Фактически это тоже самое, что и запись выше этого комментария
+    )
+  });
   return (
     <ul className="app-list list-group">
-        <EmployeesListItem/>
-        <EmployeesListItem/>
-        <EmployeesListItem/>
+        {elements}
     </ul>
   )
 }
